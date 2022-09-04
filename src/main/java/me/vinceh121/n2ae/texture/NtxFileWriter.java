55d32d83783f78130c544ca82ddeae47da3db5d9
vinceh121 <contact@vinceh121.me>
2022-09-04 14:40:24 +0200

    fix: ARGB8->ARGB4 conversion

--- src/main/java/me/vinceh121/n2ae/texture/NtxFileWriter.java
+++ src/main/java/me/vinceh121/n2ae/texture/NtxFileWriter.java
@@ -30,12 +30,12 @@ public class NtxFileWriter {
 		this.out.writeIntLE(b.getDataLength());
 	}
 
-	public static byte[] imageToRaw(BufferedImage img, BlockFormat fmt) {
+	public static byte[] imageToRaw(BufferedImage img, int width, int height, BlockFormat fmt) {
 		int[] arr = img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, img.getWidth());
-		return imageToRaw(arr, fmt);
+		return imageToRaw(arr, width, height, fmt);
 	}
 
-	public static byte[] imageToRaw(int[] argb, BlockFormat fmt) {
+	public static byte[] imageToRaw(int[] argb, int width, int height, BlockFormat fmt) {
 		if (fmt == BlockFormat.ARGB8) { // nothing, just convert to a byte array
 			ByteBuffer bytes = ByteBuffer.allocate(4 * argb.length);
 			for (int i : argb) {
@@ -55,27 +55,56 @@ public class NtxFileWriter {
 			return bytes.array();
 		} else if (fmt == BlockFormat.ARGB4) { // remove lower nibble of each component, LE
 			ByteBuffer bytes = ByteBuffer.allocate(2 * argb.length);
-			for (int i : argb) {
-				final short alpha = (short) (i >> 24 + 4);
-				final short red = (short) ((i >> 16 + 4) & 0xF);
-				final short green = (short) ((i >> 8 + 4) & 0xF);
-				final short blue = (short) ((i & 0xF) >> 4);
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
+					int idx = x * width + y;
+					int i = argb[idx];
+					// original values
+					final short oalpha = (short) (i >> 24);
+					final short ored = (short) ((i >> 16) & 0xFF);
+					final short ogreen = (short) ((i >> 8) & 0xFF);
+					final short oblue = (short) (i & 0xFF);
 
-				final short pixel = (short) (alpha << 12 | red << 8 | green << 4 | blue);
-				bytes.putShort(Short.reverseBytes(pixel));
+					// error values
+//					final short ealpha = (short) (oalpha & 0xF);
+//					final short ered = (short) (ored & 0xF);
+//					final short egreen = (short) (ogreen & 0xF);
+//					final short eblue = (short) (oblue & 0xF);
+
+					// reduced values
+					final short salpha = (short) (oalpha >> 4);
+					final short sred = (short) (ored >> 4);
+					final short sgreen = (short) (ogreen >> 4);
+					final short sblue = (short) (oblue >> 4);
+
+					final short pixel = (short) (salpha << 12 | sred << 8 | sgreen << 4 | sblue);
+					bytes.putShort(idx * 2, Short.reverseBytes(pixel));
+				}
 			}
 			return bytes.array();
 		} else if (fmt == BlockFormat.RGB565) { // remove alpha and lower 3-2-3 bits, LE
-			ByteBuffer bytes = ByteBuffer.allocate(2 * argb.length);
-			for (int i : argb) {
-				final short red = (short) ((i >> 16 + 3) & 0xF);
-				final short green = (short) ((i >> 8 + 2) & 0xF);
-				final short blue = (short) ((i & 0xF) >> 3);
+			ByteBuffer sbuf = ByteBuffer.allocate(2 * argb.length);
+			for (int i = 0; i < argb.length; i++) {
+				final int p = argb[i];
+				// original RGB values
+				final short ored = (short) ((p >> 16) & 0xFF);
+				final short ogreen = (short) ((p >> 8) & 0xFF);
+				final short oblue = (short) (p & 0xFF);
 
-				final short pixel = (short) (red << 11 | green << 5 | blue);
-				bytes.putShort(Short.reverseBytes(pixel));
+				// error values
+//				final short ered = (short) (ored & 7);
+//				final short egreen = (short) (ogreen & 3);
+//				final short eblue = (short) (oblue & 7);
+
+				// shortened values
+				short sred = (short) (ored >> 3);
+				short sgreen = (short) (ogreen >> 2);
+				short sblue = (short) (oblue >> 3);
+
+				final short pixel = (short) (sred << 11 | sgreen << 5 | sblue);
+				sbuf.putShort(Short.reverseBytes(pixel));
 			}
-			return bytes.array();
+			return sbuf.array();
 		} else {
 			throw new UnsupportedOperationException("Cannot convert Java image to raw format " + fmt);
 		}
