879a864638b0f7307ae1ce2c5f6855a40b05bd97
vinceh121 <contact@vinceh121.me>
2023-01-15 04:07:05 +0100

    feat: main gui/cli selector

--- src/main/java/me/vinceh121/n2ae/ExtractorMain.java
+++ src/main/java/me/vinceh121/n2ae/ExtractorMain.java
@@ -3,11 +3,12 @@ package me.vinceh121.n2ae;
 import java.awt.GraphicsEnvironment;
 
 import me.vinceh121.n2ae.cli.ExtractorCli;
+import me.vinceh121.n2ae.gui.ExtractorFrame;
 
 public final class ExtractorMain {
 	public static void main(String[] args) {
 		if (args.length == 0 && !GraphicsEnvironment.isHeadless()) {
-			// GUI
+			ExtractorFrame.main(args);
 		} else {
 			ExtractorCli.main(args);
 		}
