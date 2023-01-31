b2967ff2c98b472a3fb93d31bd510ba0fa3698a7
vinceh121 <contact@vinceh121.me>
2023-01-31 03:32:32 +0100

    chore: remove LWJGL dependency

--- src/main/java/me/vinceh121/n2ae/texture/BlockFormat.java
+++ src/main/java/me/vinceh121/n2ae/texture/BlockFormat.java
@@ -1,25 +1,22 @@
 package me.vinceh121.n2ae.texture;
 
-import java.awt.image.BufferedImage;
+import static me.vinceh121.n2ae.texture.GLConstants.*;
 
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
-import org.lwjgl.opengl.GL41;
+import java.awt.image.BufferedImage;
 
 public enum BlockFormat {
 	NONE(0, 0, 0, 0),
-	RGB8(BufferedImage.TYPE_INT_RGB, GL20.GL_UNSIGNED_INT, GL20.GL_RGB, GL30.GL_RGB8UI),
+	RGB8(BufferedImage.TYPE_INT_RGB, GL_UNSIGNED_INT, GL_RGB, GL_RGB8UI),
 	/**
 	 * OpenGL: swap alpha
 	 */
-	ARGB8(BufferedImage.TYPE_INT_ARGB, GL20.GL_UNSIGNED_INT_8_8_8_8, GL20.GL_RGBA, GL30.GL_RGBA8UI),
-	RGB565(BufferedImage.TYPE_USHORT_565_RGB, GL20.GL_UNSIGNED_SHORT_5_6_5, GL20.GL_RGB, GL41.GL_RGB565),
+	ARGB8(BufferedImage.TYPE_INT_ARGB, GL_UNSIGNED_INT_8_8_8_8, GL_RGBA, GL_RGBA8UI),
+	RGB565(BufferedImage.TYPE_USHORT_565_RGB, GL_UNSIGNED_SHORT_5_6_5, GL_RGB, GL_RGB565),
 	/**
 	 * JavaImage: 16-bit ARGB with 4 bits per channel, need to convert to ARGB8
 	 * OpenGL: swap alpha
 	 */
-	ARGB4(BufferedImage.TYPE_INT_ARGB, GL20.GL_UNSIGNED_SHORT_4_4_4_4, GL20.GL_RGBA, GL11.GL_RGBA4);
+	ARGB4(BufferedImage.TYPE_INT_ARGB, GL_UNSIGNED_SHORT_4_4_4_4, GL_RGBA, GL_RGBA4);
 
 	private final int javaFormat, glType, glFormat, glInternalFormat;
 
---
+++ src/main/java/me/vinceh121/n2ae/texture/GLConstants.java
@@ -0,0 +1,7 @@
+package me.vinceh121.n2ae.texture;
+
+public class GLConstants {
+	public static final int GL_RGBA4 = 0x8056, GL_UNSIGNED_SHORT_4_4_4_4 = 0x8033, GL_RGB565 = 0x8D62,
+			GL_UNSIGNED_SHORT_5_6_5 = 0x8363, GL_RGBA8UI = 0x8D7C, GL_RGBA = 0x1908, GL_UNSIGNED_INT_8_8_8_8 = 0x8035,
+			GL_RGB8UI = 0x8D7D, GL_RGB = 0x1907, GL_UNSIGNED_INT = 0x1405;
+}
